689b17167eded943de33b44264d213ecaf163ea6
reudismam <reudismam@gmail.com>
2017-03-30 15:22:01 -0300

    Refactoring.

---
+++ ProgramSynthesis/RefazerTests/TestHelper.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.ProgramSynthesis;
+using Microsoft.ProgramSynthesis.AST;
+using Microsoft.ProgramSynthesis.Specifications;
+using Microsoft.ProgramSynthesis.Utils;
+using RefazerFunctions.Substrings;
+using RefazerFunctions;
+using RefazerFunctions.Spg.Bean;
+using RefazerManager;
+using Spg.LocationRefactor.Location;
+using Spg.LocationRefactor.TextRegion;
+using Spg.LocationRefactor.Transform;
+using WorkSpaces.Spg.Workspace;
+
+namespace UnitTests
+{
+    class TestHelper
+    {
+        /// <summary>
+        /// Grammar
+        /// </summary>
+        private readonly Grammar _grammar;
+        /// <summary>
+        /// Execution id
+        /// </summary>
+        private readonly int _execId;
+        /// <summary>
+        /// Regions
+        /// </summary>
+        private readonly List<TRegion> _regions;
+        /// <summary>
+        /// Locations
+        /// </summary>
+        private readonly List<CodeLocation> _locations;
+        /// <summary>
+        /// Global transformations
+        /// </summary>
+        private readonly Dictionary<string, List<CodeTransformation>> _globalTransformations;
+        /// <summary>
+        /// Path to experiment
+        /// </summary>
+        private readonly string _expHome;
+        /// <summary>
+        /// Solution path
+        /// </summary>
+        private readonly string _solutionPath;
+        /// <summary>
+        /// Commit Id
+        /// </summary>
+        private readonly string _commit;
+        /// <summary>
+        /// Kinds
+        /// </summary>
+        private readonly List<SyntaxKind> _kinds;
+        /// <summary>
+        /// Total time to execute
+        /// </summary>
+        public long TotalTimeToExecute { get; set; }
+        /// <summary>
+        /// Total time to learn
+        /// </summary>
+        public long TotalTimeToLearn { get; set; }
+        /// <summary>
+        /// Transformed
+        /// </summary>
+        public List<object> Transformed { get; set; }
+        /// <summary>
+        /// Learned program
+        /// </summary>
+        public ProgramNode Program { get; set; }
+        /// <summary>
+        /// Dictionary selection
+        /// </summary>
+        public Dictionary<string, List<TRegion>> DictionarySelection { get; set; }
+
+        public TestHelper(Grammar grammar, List<TRegion> regions, List<CodeLocation> locations, Dictionary<string, List<CodeTransformation>> globalTransformations, string expHome, string solutionPath, string commit, List<SyntaxKind> kinds, int execId)
+        {
+            _grammar = grammar;
+            _regions = regions;
+            _locations = locations;
+            _globalTransformations = globalTransformations;
+            _expHome = expHome;
+            _solutionPath = solutionPath;
+            _commit = commit;
+            _kinds = kinds;
+            _execId = execId;
+            WorkspaceManager.GetInstance().solutionPath = _expHome + _solutionPath;
+        }
+
+        public void Execute(List<int> examples)
+        {
+            var metadataRegions = examples.Select(index => _regions[index]).ToList();
+            var locationRegions = examples.Select(index => _locations[index]).ToList();
+
+            DictionarySelection = RegionManager.GetInstance().GroupRegionBySourcePath(metadataRegions);
+            //Examples
+            var examplesInput = new List<SyntaxNodeOrToken>();
+            var examplesOutput = new List<SyntaxNodeOrToken>();
+
+            SyntaxNodeOrToken inpTree = default(SyntaxNodeOrToken);
+            //building example methods
+            var ioExamples = new Dictionary<State, IEnumerable<object>>();
+            foreach (KeyValuePair<string, List<TRegion>> entry in DictionarySelection)
+            {
+                string sourceCode = FileUtil.ReadFile(entry.Key);
+                Tuple<string, List<TRegion>> tu = Transform(sourceCode, _globalTransformations[entry.Key.ToUpperInvariant()], metadataRegions);
+                string sourceCodeAfter = tu.Item1;
+
+                inpTree = CSharpSyntaxTree.ParseText(sourceCode, path: entry.Key).GetRoot();
+                SyntaxNodeOrToken outTree = CSharpSyntaxTree.ParseText(sourceCodeAfter).GetRoot();
+
+                var allMethodsInput = GetNodesByType(inpTree, _kinds);
+                var allMethodsOutput = GetNodesByType(outTree, _kinds);
+                var inputMethods = new List<int>();
+                foreach (var region in locationRegions.Where(o => o.SourceClass.ToUpperInvariant().Equals(entry.Key.ToUpperInvariant())))
+                {
+                    for (int index = 0; index < allMethodsInput.Count; index++)
+                    {
+                        var method = allMethodsInput[index];
+                        var tRegion = new TRegion
+                        {
+                            Start = method.SpanStart,
+                            Length = method.Span.Length
+                        };
+
+                        if (region.Region.IsInside(tRegion))
+                        {
+                            inputMethods.Add(index);
+                        }
+                    }
+                }
+
+                //Examples
+                var inpExs = inputMethods.Distinct().Select(index => allMethodsInput[index]).ToList();
+                var outExs = inputMethods.Distinct().Select(index => allMethodsOutput[index]).ToList();
+
+                examplesInput.AddRange(inpExs);
+                examplesOutput.AddRange(outExs);
+            }
+
+            examplesInput = RemoveDuplicates(examplesInput);
+            for (int index = 0; index < examplesInput.Count; index++)
+            {
+                var example = examplesInput.ElementAt(index);
+                var treeNode = ConverterHelper.ConvertCSharpToTreeNode((SyntaxNodeOrToken) example);
+                var inputState = State.Create(_grammar.InputSymbol, new Node(treeNode));
+                ioExamples.Add(inputState, new List<object> { examplesOutput.ElementAt(index) });
+            }
+
+            //Learn program
+            var spec = DisjunctiveExamplesSpec.From(ioExamples);
+
+            long millBeforeLearn = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            Program = Utils.Learn(_grammar, spec, new RankingScore(_grammar),  new WitnessFunctions(_grammar));
+            long millAfterLearn = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            TotalTimeToLearn = millAfterLearn - millBeforeLearn;
+
+            var methods = new List<SyntaxNodeOrToken>();
+            if (_solutionPath == null)
+            {
+                //Run program
+                methods = GetNodesByType(inpTree, _kinds);
+            }
+            else
+            {
+                string path = _expHome + _solutionPath;
+                var files = WorkspaceManager.GetInstance().GetSourcesFiles(null, path);
+                foreach (var v in files)
+                {
+                    var tree = CSharpSyntaxTree.ParseText(v.Item1, path: v.Item2).GetRoot();
+                    var vnodes = GetNodesByType(tree, _kinds);
+                    methods.AddRange(vnodes);
+                }
+            }
+
+            Transformed = new List<object>();
+            var dicTrans = new Dictionary<string, List<object>>();
+
+            long millBeforeExecution = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            foreach (var method in methods)
+            {
+                var newInputState = State.Create(_grammar.InputSymbol, new Node(ConverterHelper.ConvertCSharpToTreeNode(method)));
+                object[] output = Program.Invoke(newInputState).ToEnumerable().ToArray();
+                Transformed.AddRange(output);
+                Utils.WriteColored(ConsoleColor.DarkCyan, output.DumpCollection(openDelim: "", closeDelim: "", separator: "\n"));
+
+                if (output.Any())
+                {
+                    if (dicTrans.ContainsKey(method.SyntaxTree.FilePath.ToUpperInvariant()))
+                    {
+                        dicTrans[method.SyntaxTree.FilePath.ToUpperInvariant()].AddRange(output);
+                    }
+                    else
+                    {
+                        dicTrans[method.SyntaxTree.FilePath.ToUpperInvariant()] = output.ToList();
+                    }
+                }
+            }
+            long millAfterExecution = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            TotalTimeToExecute = millAfterExecution - millBeforeExecution;
+
+            string s = "";
+            foreach (var v in dicTrans)
+            {
+                s += $"{v.Key} \n====================\n";
+                v.Value.ForEach(o => s += $"{o}\n");
+            }
+
+            Console.WriteLine($"Count: \n {Transformed.Count}");
+            s += $"Count: \n {Transformed.Count}";
+            FileUtil.WriteToFile(_expHome + @"cprose\" + _commit + @"\result"+_execId+".res", s);
+        }
+
+        private static List<SyntaxNodeOrToken> GetNodesByType(SyntaxNodeOrToken outTree, List<SyntaxKind> kinds)
+        {
+            //select nodes of type method declaration
+            var exampleMethodsInput = from inode in outTree.AsNode().DescendantNodesAndSelf()
+                where kinds.Where(inode.IsKind).Any()
+                select inode;
+            //Select two examples
+            var examplesSotInput = exampleMethodsInput.Select(sot => (SyntaxNodeOrToken)sot).ToList();
+            //.GetRange(0, 1);
+            //var examplesInput = examplesSotInput.Select(o => (object)o).ToList();
+            return examplesSotInput;
+        }
+
+        private static List<SyntaxNodeOrToken> RemoveDuplicates(List<SyntaxNodeOrToken> methods)
+        {
+            var removes = new List<SyntaxNodeOrToken>();
+            for (int i = 0; i < methods.Count; i++)
+            {
+                for (int j = 0; j < methods.Count; j++)
+                {
+                    if (i != j &&
+                        methods[i].SyntaxTree.FilePath.ToUpperInvariant()
+                            .Equals(methods[j].SyntaxTree.FilePath.ToUpperInvariant()) &&
+                        methods[i].Span.Contains(methods[j].Span))
+                    {
+                        removes.Add(methods[j]);
+                    }
+                }
+            }
+
+            var result = methods.Except(removes).ToList();
+            return result;
+        }
+
+        public static Tuple<string, List<TRegion>> Transform(string source, List<CodeTransformation> transformations, List<TRegion> regions)
+        {
+            List<TRegion> tRegions = new List<TRegion>();
+            int nextStart = 0;
+            string sourceCode = source;
+            foreach (CodeTransformation item in transformations)
+            {
+                Tuple<TRegion, TRegion> tregion = GetTRegionShift(regions, item);
+                TRegion region = tregion.Item1;
+                string transformation = tregion.Item2.Text;
+
+                int start = nextStart + region.Start;
+                int end = start + region.Length;
+                var sourceCodeUntilStart = sourceCode.Substring(0, start);
+                var sourceCodeAfterSelection = sourceCode.Substring(end);
+                sourceCode = sourceCodeUntilStart + transformation + sourceCodeAfterSelection;
+
+                TRegion tr = new TRegion();
+                tr.Start = start - 1;
+                tr.Length = tregion.Item2.Length + 2;
+                tr.Text = tregion.Item2.Text;
+                tr.Path = tregion.Item1.Path;
+                tRegions.Add(tr);
+
+                nextStart += transformation.Length - region.Length;
+            }
+            Tuple<string, List<TRegion>> t = Tuple.Create(sourceCode, tRegions);
+            return t;
+            //return sourceCode;
+        }
+
+        private static Tuple<TRegion, TRegion> GetTRegionShift(List<TRegion> regions, CodeTransformation codeTransformation)
+        {
+            Tuple<TRegion, TRegion> t;
+            foreach (TRegion tr in regions)
+            {
+                if (codeTransformation.Trans.Equals(tr))
+                {
+                    TRegion region = new TRegion();
+                    region.Start = tr.Start;
+                    region.Length = tr.Length - 2;
+                    region.Path = tr.Path;
+                    region.Text = tr.Text;
+                    t = Tuple.Create(codeTransformation.Location.Region, region);
+                    return t;
+                }
+            }
+            t = Tuple.Create(codeTransformation.Location.Region, codeTransformation.Location.Region);
+            return t;
+        }
+    }
+}
